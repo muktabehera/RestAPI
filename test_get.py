dc7ddfdfa224ef0396778bb06ea393e9949c83da
Mukta Behera <mbehera@qumu.com>
2020-10-22 11:39:26 -0700

    added new file

---
+++ test_get.py
@@ -0,0 +1,12 @@
+import requests
+import json
+
+def test_getPet():
+    version = 2
+    # base_url = 'https://petstore.swagger.io/v' + str(version)
+    #
+    # pet_response = requests.get(base_url+pet_url)
+    # print(pet_response.text)
+    # print(pet_response.status_code)
+    # return pet_response.text
+    print("HELLO")
